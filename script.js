659b30b3afa1d3592f32ec68c43e1440c7f55a8d
DylanSantiago <2dylansantiago@gmail.com>
2022-11-22 06:50:23 -0500

    creating sections for time blocks

---
+++ script.js
@@ -0,0 +1,19 @@
+var moment = moment();
+var today = $("#currentDay");
+
+var timeSlot = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
+var militaryTime = [09, 10, 11, 12, 13, 14, 15, 16, 17];
+
+var timeBlock;
+var hourBlock;
+var plansBlock;
+var saveBtn;
+
+const planDay = () => {
+    $.each(time, function (array, time) {
+        timeBlock = $("<section>");
+        timeBlock.addClass("row");
+
+    }
+    
+    )}
